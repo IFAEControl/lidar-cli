15f3fcce9a03d75b498b2408beb68d6075f25781
David Roman <droman@ifae.es>
2019-06-17 12:39:52 +0200

    monitoring: getLastValue

--- src/main/java/cat/ifae/cta/lidar/control/cli/commands/Monitoring.java
+++ src/main/java/cat/ifae/cta/lidar/control/cli/commands/Monitoring.java
@@ -11,6 +11,10 @@ import java.util.concurrent.CountDownLatch;
 public
 class Monitoring implements Runnable {
     private MonitoringGrpc.MonitoringStub stub;
+    private MonitoringGrpc.MonitoringBlockingStub blockingStub;
+
+    @CommandLine.Option(names = "-last-value", description = "Get last value")
+    private boolean last_value = false;
 
     @CommandLine.ParentCommand
     private Licli parent;
@@ -20,31 +24,45 @@ class Monitoring implements Runnable {
         stub = MonitoringGrpc.newStub(parent.sm.getCh());
         stub = parent.sm.addMetadata(stub);
 
+        blockingStub = MonitoringGrpc.newBlockingStub(parent.sm.getCh());
+        blockingStub = parent.sm.addMetadata(blockingStub);
+
         CommandLine.populateCommand(this);
 
         try {
             //var requestObserverRef = new AtomicReference<>();
-            var finishedLatch = new CountDownLatch(1);
-            Str req = Str.newBuilder().setText("some_variable").build();
-            stub.watchVariable(req, new StreamObserver<Str>() {
-                public void onNext(Str response) {
-                    System.out.print(response);
-                }
-
-                public void onError(Throwable t) {
-                    System.out.println("Error: " + t.getMessage());
-                    finishedLatch.countDown();
-                }
-
-                public void onCompleted() {
-                    finishedLatch.countDown();
-                }
-            });
-
-            finishedLatch.await();
+            if(last_value) getLastValue();
+            else monitoring();
         } catch(InterruptedException | RuntimeException e) {
             System.out.println(e.toString());
         }
 
+    }
+
+    private void getLastValue() {
+        var request = Str.newBuilder().setText("temperature").build();
+        var resp = blockingStub.getLastValue(request);
+        System.out.println(resp.getText());
+    }
+
+    private void monitoring() throws InterruptedException {
+        var finishedLatch = new CountDownLatch(1);
+        Str req = Str.newBuilder().setText("temperature").build();
+        stub.watchVariable(req, new StreamObserver<Str>() {
+            public void onNext(Str response) {
+                System.out.print(response);
+            }
+
+            public void onError(Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+                finishedLatch.countDown();
+            }
+
+            public void onCompleted() {
+                finishedLatch.countDown();
+            }
+        });
+
+        finishedLatch.await();
     }
 }
