9ddcd2f3422ef6f5fb142278666b0471f42b7614
David Roman <droman@ifae.es>
2019-06-18 12:08:55 +0200

    monitoring changes

--- src/main/java/cat/ifae/cta/lidar/control/cli/commands/Monitoring.java
+++ src/main/java/cat/ifae/cta/lidar/control/cli/commands/Monitoring.java
@@ -13,6 +13,10 @@ class Monitoring implements Runnable {
     private MonitoringGrpc.MonitoringStub stub;
     private MonitoringGrpc.MonitoringBlockingStub blockingStub;
 
+    @CommandLine.Parameters(paramLabel = "NAME", description = "Name")
+    private String name;
+
+
     @CommandLine.Option(names = "-last-value", description = "Get last value")
     private boolean last_value = false;
 
@@ -31,23 +35,23 @@ class Monitoring implements Runnable {
 
         try {
             //var requestObserverRef = new AtomicReference<>();
-            if(last_value) getLastValue();
-            else monitoring();
+            if(last_value) getLastValue(name);
+            else monitoring(name);
         } catch(InterruptedException | RuntimeException e) {
             System.out.println(e.toString());
         }
 
     }
 
-    private void getLastValue() {
-        var request = Str.newBuilder().setText("temperature").build();
+    private void getLastValue(String m) {
+        var request = Str.newBuilder().setText(m).build();
         var resp = blockingStub.getLastValue(request);
         System.out.println(resp.getText());
     }
 
-    private void monitoring() throws InterruptedException {
+    private void monitoring(String m) throws InterruptedException {
         var finishedLatch = new CountDownLatch(1);
-        Str req = Str.newBuilder().setText("temperature").build();
+        Str req = Str.newBuilder().setText(m).build();
         stub.watchVariable(req, new StreamObserver<Str>() {
             public void onNext(Str response) {
                 System.out.print(response);
