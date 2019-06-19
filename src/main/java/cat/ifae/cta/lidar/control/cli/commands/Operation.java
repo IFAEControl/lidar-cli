2172ad2cda29b05c6452ca07050d19ccb2aadae0
David Roman <droman@ifae.es>
2019-06-19 16:10:41 +0200

    add stop telescope tests

--- src/main/java/cat/ifae/cta/lidar/control/cli/commands/Operation.java
+++ src/main/java/cat/ifae/cta/lidar/control/cli/commands/Operation.java
@@ -11,40 +11,60 @@ import java.util.concurrent.CountDownLatch;
 
 @CommandLine.Command(name = "operation", mixinStandardHelpOptions = true)
 public class Operation implements Runnable {
+    @CommandLine.Option(names = "-telescope-test", description = "Execute telescope test")
+    private boolean telescope_test = false;
+
+    @CommandLine.Option(names = "-stop-telescope-test", description = "Stop telescope test")
+    private boolean stop_telescope_test = false;
+
     @CommandLine.ParentCommand
     private Licli parent;
 
     private OperationGrpc.OperationStub stub;
+    private OperationGrpc.OperationBlockingStub blocking_stub;
 
     @Override
     public void run() {
         stub = OperationGrpc.newStub(parent.sm.getCh());
         stub = parent.sm.addMetadata(stub);
 
+        blocking_stub = OperationGrpc.newBlockingStub(parent.sm.getCh());
+        blocking_stub = parent.sm.addMetadata(blocking_stub);
+
         CommandLine.populateCommand(this);
 
         try {
-            var finishedLatch = new CountDownLatch(1);
-
-            Null req = Null.newBuilder().build();
-            stub.executeTelescopTests(req, new StreamObserver<>() {
-                public void onNext(EncoderPosition response) {
-                    System.out.println(response.getName() + ": " + response.getPosition());
-                }
-
-                public void onError(Throwable t) {
-                    System.out.println("Error: " + t.getMessage());
-                    finishedLatch.countDown();
-                }
-
-                public void onCompleted() {
-                    finishedLatch.countDown();
-                }
-            });
-
-            finishedLatch.await();
+            if(telescope_test) executeTelescopeTests();
+            if(stop_telescope_test) stopTelescopeTests();
         } catch(Exception e) {
             System.out.println(e.toString());
         }
+    }
+
+    private void executeTelescopeTests() throws InterruptedException {
+        var finishedLatch = new CountDownLatch(1);
+
+        Null req = Null.newBuilder().build();
+        stub.executeTelescopeTests(req, new StreamObserver<>() {
+            public void onNext(EncoderPosition response) {
+                System.out.println(response.getName() + ": " + response.getPosition());
+            }
+
+            public void onError(Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+                finishedLatch.countDown();
+            }
+
+            public void onCompleted() {
+                finishedLatch.countDown();
+            }
+        });
+
+        finishedLatch.await();
+    }
+
+    private void stopTelescopeTests() {
+        Null req = Null.newBuilder().build();
+        blocking_stub.stopTelescopeTests(req);
     }
 }
