bd7e49574b36c6305b7685ccb2fee2402fabc316
David Roman <droman@ifae.es>
2019-11-05 15:33:44 +0100

    add version information

--- src/main/java/cat/ifae/cta/lidar/control/cli/Licli.java
+++ src/main/java/cat/ifae/cta/lidar/control/cli/Licli.java
@@ -9,7 +9,9 @@ import org.apache.logging.log4j.core.config.Configurator;
 import picocli.CommandLine;
 
 
-@CommandLine.Command(mixinStandardHelpOptions = true, subcommands = {LLControl.class, Licel.class,
+@CommandLine.Command(version = "0.0.1", mixinStandardHelpOptions = true, subcommands =
+        {LLControl.class,
+        Licel.class,
         Motors.class, Monitoring.class, Operation.class, Alarms.class, Config.class, Trace.class})
 public class Licli implements Runnable {
     public static SessionManager sm = new SessionManager();
